43227d2e1c3b02391fb03c45fbfe01e5948e5ed5
aljoharah <aljoharrakh@hotmail.com>
2019-08-21 08:32:30 +0300

    Second Day

---
+++ Day2.py
@@ -0,0 +1,14 @@
+"""
+
+اليوم الثاني من رحلة 100  يوم برمجة
+(5)المقامة من قبل المبادرة السعودية للمطورين
+
+"""
+#Wednesday
+#21 August
+#first statement(1)(4)
+print('Hello, World!')
+#first statement(1)
+if 5>2:
+    print("Five is greater than two!")#printed if condition is met(2)
+#print('end code')(3)
