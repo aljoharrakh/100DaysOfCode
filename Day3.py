11178f5a0e84152a66d22975ed604a42fab3dbcf
aljoharah <aljoharrakh@hotmail.com>
2019-08-23 01:42:49 +0300

    Third Day

---
+++ Day3.py
@@ -0,0 +1,8 @@
+sent1,sent2='Today is the third',' day of the Saudi Developers Initiative, the date is'
+day=22
+month="August"
+info=sent1+sent2
+print(info,day,month)
+info="I wish you good luck! , "
+x=y='bye '
+print(info+x+y)
