24e766fbe059ab8e186cd53bc788765db989859d
aljoharah <aljoharrakh@hotmail.com>
2019-08-31 20:45:06 +0300

    Tenth Day

---
+++ Day10.py
@@ -0,0 +1,14 @@
+num1=5
+num2=10
+print("***** Arithmetic *****")
+print("The first number multiplied by the second number equals ",num1*num2)
+print("***** Assignment *****")
+total=0
+total+=num1
+total+=num2
+print("Total numeric is equal to", total)
+print("***** Comparison *****")
+if num1>num2 :
+    print(" number 1 is greater than number 2")
+else:
+    print(" number 2 is greater than number 1")
