d24404c2591ab54912a521a35e9daa31c6147c03
aljoharah <aljoharrakh@hotmail.com>
2019-08-31 20:58:53 +0300

    Eleventh Day

---
+++ Day11.py
@@ -0,0 +1,8 @@
+num=9
+arrNum=[1,5,8]
+print("***** Logical *****")
+print(8<num and 9==num)
+print("***** Identity *****")
+print(arrNum is num )
+print("***** Membership *****")
+print (5 in arrNum
